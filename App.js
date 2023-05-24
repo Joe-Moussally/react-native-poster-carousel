b12722ae8f2885b589c48945f4a38db4e5eecf58
Joe-Moussally <joemoussally111@gmail.com>
2023-05-24 04:16:56 +0300

    [Image]: Creating basic carousel flatlist

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import {
+  Dimensions,
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  View
+} from "react-native"
+
+const posters = [
+  require("./posters/poster-1.webp"),
+  require("./posters/poster-2.jpeg"),
+  require("./posters/poster-3.jpeg"),
+  require("./posters/poster-4.webp"),
+  require("./posters/poster-5.webp")
+]
+
+const { width, height } = Dimensions.get("screen")
+const imageWidth = width * 0.7
+const imageHeight = height * 1.54
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "black" }}>
+      <StatusBar hidden />
+      <FlatList
+        data={posters}
+        key={(_, index) => index.toString}
+        renderItem={({ item: poster }) => (
+          <Image
+            source={poster}
+            style={{
+              width: imageWidth,
+              height: imageWidth,
+              resizeMode: "cover"
+            }}
+          />
+        )}
+      />
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
